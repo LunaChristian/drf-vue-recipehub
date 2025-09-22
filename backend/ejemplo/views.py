2fbc6abdee8697b100c82155d1fcb44ea56963df
Insert coin <christianlunaster1@gmail.com>
2025-09-22 14:48:38 +0200

    Clase 17: Implementación de múltiples métodos HTTP en APIView (GET, POST, PUT, DELETE)

--- backend/ejemplo/views.py
+++ backend/ejemplo/views.py
@@ -5,3 +5,12 @@ from django.http import HttpResponse
 class Class_Ejemplo_View(APIView):
     def get(self, request):
         return HttpResponse("Texto de ejemplo")
+
+    def post(self, request):
+        return HttpResponse("Salida con el método POST")
+
+    def put(self, request):
+        return HttpResponse("Salida con el método PUT")
+
+    def delete(self, request):
+        return HttpResponse("Salida con el método DELETE")
