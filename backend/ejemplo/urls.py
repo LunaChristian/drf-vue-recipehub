cb61a9ce818a69c34de67b9bf8aa7cd176201b72
Insert coin <christianlunaster1@gmail.com>
2025-09-22 16:36:14 +0200

    Clase 18: Manejo de parámetros en rutas y separación de clases APIView (Ejemplo y EjemploParametros)

--- backend/ejemplo/urls.py
+++ backend/ejemplo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Class_Ejemplo_View
+from .views import Class_Ejemplo_View, Class_Ejemplo_View_Parametros
 
 urlpatterns = [
     path('ejemplo', Class_Ejemplo_View.as_view()),
+    path('ejemplo/<int:id>', Class_Ejemplo_View_Parametros.as_view(), name="ejemplo_parametros")
 ]
--- backend/ejemplo/views.py
+++ backend/ejemplo/views.py
@@ -14,3 +14,16 @@ class Class_Ejemplo_View(APIView):
 
     def delete(self, request):
         return HttpResponse("Salida con el método DELETE")
+    
+class Class_Ejemplo_View_Parametros(APIView):
+    def get(self, request, id: int):
+        return HttpResponse(f"Salida con el método GET | parametro={id}")
+
+    def post(self, request, id: int):
+        return HttpResponse(f"Salida con el método POST | parametro={id}")
+
+    def put(self, request, id: int):
+        return HttpResponse(f"Salida con el método PUT | parametro={id}")
+
+    def delete(self, request, id: int):
+        return HttpResponse(f"Salida con el método DELETE | parametro={id}")
