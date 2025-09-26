5db187440e55a62c89bd34ccea307a83aa86fe70
Insert coin <christianlunaster1@gmail.com>
2025-09-27 00:34:07 +0200

    Clase 23: Configuración de archivos estáticos (assets) y media (upload) en settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 from dotenv import load_dotenv
@@ -119,7 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/assets/'
+STATIC_FILES_DIRS = (
+    os.path.join(BASE_DIR, 'assets'),
+)
+MEDIA_URLS = '/uploads/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
