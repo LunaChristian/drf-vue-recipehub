0004878c6b6c7c963413d63904a16d8282d49ff9
Insert coin <christianlunaster1@gmail.com>
2025-09-30 23:44:22 +0200

    Clase 29: Configuración de base de datos MySQL en Django (ajustes en settings.py con DATABASES, creación de BD local y prueba de conexión)

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -79,9 +79,16 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "recetarios_django_drf",
+        "USER": "root",
+        "PASSWORD": "",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
+        "OPTIONS": {
+            "autocommit": True
+        }
     }
 }
 
