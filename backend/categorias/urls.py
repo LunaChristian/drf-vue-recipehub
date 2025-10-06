a0a109ad197aac441c8351cdcff8c4cb538b141c
Insert coin <christianlunaster1@gmail.com>
2025-10-07 00:50:39 +0200

    Clase 37: Endpoint GET /api/v1/categorias/<id>. Consulta con ORM .get(id=...), respuesta JsonResponse con objeto plano y manejo de excepciones (Categoria.DoesNotExist → Http404).

--- backend/categorias/urls.py
+++ backend/categorias/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Class_Categoria_View
+from .views import Class_Categoria_View, Class_Categoria_View2
 
 urlpatterns = [
     path('categorias', Class_Categoria_View.as_view()),
+    path('categorias/<int:id>', Class_Categoria_View2.as_view()),
 ]
--- backend/categorias/views.py
+++ backend/categorias/views.py
@@ -1,3 +1,5 @@
+
+from http import HTTPStatus
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.http import Http404, HttpResponse, JsonResponse
@@ -12,4 +14,12 @@ class Class_Categoria_View(APIView):
     def get(self, request):
         data = Categoria.objects.order_by('-id').all()
         data_json = CategoriaSerializer(data, many=False)
-        return JsonResponse(data_json.data)
+        return JsonResponse({"data":data_json.data}, status=HTTPStatus.OK)
+    
+class Class_Categoria_View2(APIView):
+    def get(self, request, id):
+        try:
+            data = Categoria.objects.filter(pk=id).get()
+            return JsonResponse({"data": {"id": data.id, "titulo":data.titulo, "slug":data.slug}}, status=HTTPStatus.OK)
+        except Categoria.DoesNotExist:
+            raise Http404
