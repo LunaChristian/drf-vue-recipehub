87bcba23f0d4cb1d9383310b13e29229a2ae23d5
Insert coin <christianlunaster1@gmail.com>
2025-10-07 20:24:08 +0200

    Clase 45: Implementación del método GET en la vista Class_Receta_View para listar recetas. Uso de RecetaSerializer y JsonResponse. Próximas mejoras en formateo de datos y relaciones.

--- backend/recetas/views.py
+++ backend/recetas/views.py
@@ -2,13 +2,28 @@ from http import HTTPStatus
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.http import Http404, HttpResponse, JsonResponse
+
+from .serializers import RecetaSerializer
 from .models import Receta
 from django.utils.text import slugify
 
 # Create your views here.
 class Class_Receta_View(APIView):
    
-    pass
+    def get(self, request):
+        data = Receta.objects.order_by('-id').all()
+        data_json = RecetaSerializer(data, many=True)
+        return JsonResponse({"data":data_json.data}, status=HTTPStatus.OK)
+    
+    """def post(self, request):
+        if request.data.get('titulo')==None or not request.data['titulo']:
+            return JsonResponse({"estado":"error", "mensaje":"campo obligatorio"}, status=HTTPStatus.BAD_REQUEST)
+        try:
+            Receta.objects.create(titulo=request.data['titulo'])
+            return JsonResponse({"estado":"ok", "mensaje":"registro exitoso"}, status=HTTPStatus.CREATED)
+        except Exception as e:
+            print(f'error: {e}')
+            raise Http404 """
     
 class Class_Receta_View2(APIView):   
     
