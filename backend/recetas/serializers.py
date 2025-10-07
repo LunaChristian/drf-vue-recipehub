9fd299c38a2b9a7c1f055c449ed527ff925fde77
Insert coin <christianlunaster1@gmail.com>
2025-10-07 20:49:51 +0200

    Clase 47: Agregado campo calculado 'imagen' en RecetaSerializer con SerializerMethodField. Construcción dinámica de URL absoluta usando BASE_URL del .env.

--- backend/recetas/serializers.py
+++ backend/recetas/serializers.py
@@ -1,12 +1,16 @@
 from rest_framework import serializers
 from .models import Receta
+import os
 
 class RecetaSerializer(serializers.ModelSerializer):
     
     categoria = serializers.ReadOnlyField(source='categoria.titulo')
     fecha = serializers.DateTimeField(format="%d/%m/%Y")
+    imagen = serializers.SerializerMethodField()
     
     class Meta:
         model = Receta
-        fields = ('id', 'titulo_receta', 'slug', 'tiempo', 'descripcion', 'fecha', 'categoria', 'categoria_id')
-        #fields = '__all__'
+        fields = ('id', 'titulo_receta', 'slug', 'tiempo', 'descripcion', 'fecha', 'categoria', 'categoria_id', 'imagen')
+    
+    def get_imagen(sefl, obj):
+        return f"{os.getenv("BASE_URL")}uploads/recetas/{obj.foto}"
