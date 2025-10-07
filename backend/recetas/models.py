2ba7c4298e26d565f297ff2e63d70c5bdbc386fb
Insert coin <christianlunaster1@gmail.com>
2025-10-07 19:59:06 +0200

    Clase 43: Creación del modelo Receta con relación ForeignKey a Categoría. Campos: titulo_receta, slug, tiempo, foto, descripción, fecha. Registro de app en settings.py y ejecución de migraciones.

--- backend/recetas/models.py
+++ backend/recetas/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 from autoslug import AutoSlugField
+from categorias.models import Categoria
 
 # Create your models here.
 class Receta(models.Model):
+    categoria = models.ForeignKey(Categoria, models.DO_NOTHING)
     titulo_receta = models.CharField(max_length=100, null=False)
+    slug = AutoSlugField(populate_from='titulo_receta', max_length=100)
+    tiempo = models.CharField(max_length=100, null=True)
+    foto = models.CharField(max_length=100, null=True)
+    descripcion = models.TextField()
+    fecha = models.DateTimeField(auto_now=True)
+    
     
     
     def __str__(self):
