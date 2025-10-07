885a2bf0c012768d465fa999f3258731630c452a
Insert coin <christianlunaster1@gmail.com>
2025-10-07 20:35:09 +0200

    Clase 46: Mejora del RecetaSerializer. Campos personalizados (categoria legible via source), fecha formateada (%d/%m/%Y) y listado de campos explícito.

--- backend/recetas/serializers.py
+++ backend/recetas/serializers.py
@@ -2,7 +2,11 @@ from rest_framework import serializers
 from .models import Receta
 
 class RecetaSerializer(serializers.ModelSerializer):
+    
+    categoria = serializers.ReadOnlyField(source='categoria.titulo')
+    fecha = serializers.DateTimeField(format="%d/%m/%Y")
+    
     class Meta:
         model = Receta
-        #fields = ('id', 'titulo', 'slug')
-        fields = '__all__'
+        fields = ('id', 'titulo_receta', 'slug', 'tiempo', 'descripcion', 'fecha', 'categoria', 'categoria_id')
+        #fields = '__all__'
